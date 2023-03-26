c3598f1ad3bc468ce838a49b95fd08bd6797fc70
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-03-26 16:02:39 +0100

    Create BanList.lua

---
+++ BanList.lua
@@ -0,0 +1,23 @@
+HardwareIDban = {{'HardWareId','Reson'}}
+ProfileIDban = {{'ProfileNumbers','Reson'}}
+
+-- Edit Above
+
+BanListFuncs = {}
+BanListFuncs.ReHardwareBan = function()
+for i,v in pairs(HardwareIDban) do
+if v[1] == game:GetService("RbxAnalyticsService"):GetClientId() then
+return {true,v[2]}
+end
+end
+end
+
+BanListFuncs.ReProfileIDban = function()
+for i,v in pairs(ProfileIDban) do
+if v[1] == game.Players.LocalPlayer.UserID then
+return {true,v[2]}
+end
+end
+end
+
+return BanListFuncs
